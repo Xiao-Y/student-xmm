e903d36bd3941c7235901e24e70c85293e9abf7f
Xiao-Y <lyongtao123@126.com>
2017-11-16 10:44:53 +0800

    优化邮件发送为多线程，提高发送速度

--- student-common/src/main/java/org/billow/common/email/EmailServer.java
+++ student-common/src/main/java/org/billow/common/email/EmailServer.java
@@ -41,159 +41,170 @@ public class EmailServer {
      * @param subject  邮件主题
      * @param content  邮件内容
      */
-    public void singleMailSend(String toEmails, String subject, String content) {
-        try {
-            logger.info("------------开始发送邮件----------");
-            // 建立邮件消息
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-            // 设置发件人邮箱
-            messageHelper.setFrom(simpleMailMessage.getFrom());
-            // 设置收件人邮箱
-            if (ToolsUtils.isNotEmpty(toEmails)) {
-                String[] toEmailArray = toEmails.split(";");
-                List<String> toEmailList = new ArrayList<>();
-                if (ToolsUtils.isEmpty(toEmailArray)) {
-                    throw new SimpleException("收件人邮箱不得为空！");
-                } else {
-                    for (String s : toEmailArray) {
-                        //过滤收件人为空的
-                        if (ToolsUtils.isNotEmpty(s)) {
-                            toEmailList.add(s);
+    public void singleMailSend(final String toEmails, final String subject, final String content) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    logger.info("------------开始发送邮件----------");
+                    // 建立邮件消息
+                    MimeMessage message = javaMailSender.createMimeMessage();
+                    MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+                    // 设置发件人邮箱
+                    messageHelper.setFrom(simpleMailMessage.getFrom());
+                    // 设置收件人邮箱
+                    if (ToolsUtils.isNotEmpty(toEmails)) {
+                        String[] toEmailArray = toEmails.split(";");
+                        List<String> toEmailList = new ArrayList<>();
+                        if (ToolsUtils.isEmpty(toEmailArray)) {
+                            throw new SimpleException("收件人邮箱不得为空！");
+                        } else {
+                            for (String s : toEmailArray) {
+                                //过滤收件人为空的
+                                if (ToolsUtils.isNotEmpty(s)) {
+                                    toEmailList.add(s);
+                                }
+                            }
+                            if (ToolsUtils.isEmpty(toEmailList)) {
+                                throw new SimpleException("收件人邮箱不得为空！");
+                            } else {
+                                toEmailArray = toEmailList.toArray(new String[toEmailList.size()]);
+                            }
                         }
-                    }
-                    if (ToolsUtils.isEmpty(toEmailList)) {
-                        throw new SimpleException("收件人邮箱不得为空！");
+                        messageHelper.setTo(toEmailArray);
                     } else {
-                        toEmailArray = toEmailList.toArray(new String[toEmailList.size()]);
+                        //默认的
+                        messageHelper.setTo(simpleMailMessage.getTo());
                     }
+                    // 邮件主题
+                    if (ToolsUtils.isNotEmpty(subject)) {
+                        messageHelper.setSubject(subject);
+                    } else {
+                        //默认的
+                        messageHelper.setSubject(simpleMailMessage.getSubject());
+                    }
+                    // true 表示启动HTML格式的邮件
+                    if (ToolsUtils.isNotEmpty(content)) {
+                        messageHelper.setText(content, true);
+                    } else {
+                        messageHelper.setText(simpleMailMessage.getText(), true);
+                    }
+                    messageHelper.setSentDate(new Date());
+                    // 发送邮件
+                    javaMailSender.send(message);
+                    logger.info("邮件主题-->" + subject);
+                    logger.info("邮件内容-->" + content);
+                    logger.info("------------发送邮件完成----------");
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                    logger.info("------------发送邮件异常----------");
                 }
-                messageHelper.setTo(toEmailArray);
-            } else {
-                //默认的
-                messageHelper.setTo(simpleMailMessage.getTo());
             }
-            // 邮件主题
-            if (ToolsUtils.isNotEmpty(subject)) {
-                messageHelper.setSubject(subject);
-            } else {
-                //默认的
-                messageHelper.setSubject(simpleMailMessage.getSubject());
-            }
-            // true 表示启动HTML格式的邮件
-            if (ToolsUtils.isNotEmpty(content)) {
-                messageHelper.setText(content, true);
-            } else {
-                messageHelper.setText(simpleMailMessage.getText(), true);
-            }
-            messageHelper.setSentDate(new Date());
-            // 发送邮件
-            javaMailSender.send(message);
-            logger.info("邮件主题-->" + subject);
-            logger.info("邮件内容-->" + content);
-            logger.info("------------发送邮件完成----------");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            logger.info("------------发送邮件异常----------");
-        }
+        }).start();
     }
 
     /**
-     * 发送邮件
+     * 发送复杂邮件(图片/附件)
      *
      * @param mail
      */
-    public void sendEmail(MailModel mail) {
-        try {
-            logger.info("------------开始发送邮件----------");
-            // 建立邮件消息
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-            // 设置发件人邮箱
-            if (mail.getEmailFrom() != null) {
-                messageHelper.setFrom(mail.getEmailFrom());
-            } else {
-                messageHelper.setFrom(simpleMailMessage.getFrom());
-            }
-            // 设置收件人邮箱
-            if (ToolsUtils.isNotEmpty(mail.getToEmails())) {
-                String[] toEmailArray = mail.getToEmails().split(";");
-                List<String> toEmailList = new ArrayList<>();
-                if (ToolsUtils.isEmpty(toEmailArray)) {
-                    throw new SimpleException("收件人邮箱不得为空！");
-                } else {
-                    for (String s : toEmailArray) {
-                        //过滤收件人为空的
-                        if (ToolsUtils.isNotEmpty(s)) {
-                            toEmailList.add(s);
+    public void sendEmail(final MailModel mail) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    logger.info("------------开始发送邮件----------");
+                    // 建立邮件消息
+                    MimeMessage message = javaMailSender.createMimeMessage();
+                    MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+                    // 设置发件人邮箱
+                    if (mail.getEmailFrom() != null) {
+                        messageHelper.setFrom(mail.getEmailFrom());
+                    } else {
+                        messageHelper.setFrom(simpleMailMessage.getFrom());
+                    }
+                    // 设置收件人邮箱
+                    if (ToolsUtils.isNotEmpty(mail.getToEmails())) {
+                        String[] toEmailArray = mail.getToEmails().split(";");
+                        List<String> toEmailList = new ArrayList<>();
+                        if (ToolsUtils.isEmpty(toEmailArray)) {
+                            throw new SimpleException("收件人邮箱不得为空！");
+                        } else {
+                            for (String s : toEmailArray) {
+                                //过滤收件人为空的
+                                if (ToolsUtils.isNotEmpty(s)) {
+                                    toEmailList.add(s);
+                                }
+                            }
+                            if (ToolsUtils.isEmpty(toEmailList)) {
+                                throw new SimpleException("收件人邮箱不得为空！");
+                            } else {
+                                toEmailArray = toEmailList.toArray(new String[toEmailList.size()]);
+                            }
+                        }
+                        messageHelper.setTo(toEmailArray);
+                    } else {
+                        //默认的
+                        messageHelper.setTo(simpleMailMessage.getTo());
+                    }
+
+                    // 邮件主题
+                    if (ToolsUtils.isNotEmpty(mail.getSubject())) {
+                        messageHelper.setSubject(mail.getSubject());
+                    } else {
+                        messageHelper.setSubject(simpleMailMessage.getSubject());
+                    }
+
+                    // true 表示启动HTML格式的邮件
+                    if (ToolsUtils.isNotEmpty(mail.getContent())) {
+                        messageHelper.setText(mail.getContent(), true);
+                    } else {
+                        messageHelper.setText(simpleMailMessage.getText(), true);
+                    }
+
+                    // 添加图片
+                    if (null != mail.getPictures()) {
+                        for (Map.Entry<String, String> entry : mail.getPictures().entrySet()) {
+                            String cid = entry.getKey();
+                            String filePath = entry.getValue();
+                            if (null == cid || null == filePath) {
+                                throw new RuntimeException("请确认每张图片的ID和图片地址是否齐全！");
+                            }
+                            File file = new File(filePath);
+                            if (!file.exists()) {
+                                throw new RuntimeException("图片" + filePath + "不存在！");
+                            }
+                            FileSystemResource img = new FileSystemResource(file);
+                            messageHelper.addInline(cid, img);
                         }
                     }
-                    if (ToolsUtils.isEmpty(toEmailList)) {
-                        throw new SimpleException("收件人邮箱不得为空！");
-                    } else {
-                        toEmailArray = toEmailList.toArray(new String[toEmailList.size()]);
-                    }
-                }
-                messageHelper.setTo(toEmailArray);
-            } else {
-                //默认的
-                messageHelper.setTo(simpleMailMessage.getTo());
-            }
 
-            // 邮件主题
-            if (ToolsUtils.isNotEmpty(mail.getSubject())) {
-                messageHelper.setSubject(mail.getSubject());
-            } else {
-                messageHelper.setSubject(simpleMailMessage.getSubject());
-            }
-
-            // true 表示启动HTML格式的邮件
-            if (ToolsUtils.isNotEmpty(mail.getContent())) {
-                messageHelper.setText(mail.getContent(), true);
-            } else {
-                messageHelper.setText(simpleMailMessage.getText(), true);
-            }
-
-            // 添加图片
-            if (null != mail.getPictures()) {
-                for (Map.Entry<String, String> entry : mail.getPictures().entrySet()) {
-                    String cid = entry.getKey();
-                    String filePath = entry.getValue();
-                    if (null == cid || null == filePath) {
-                        throw new RuntimeException("请确认每张图片的ID和图片地址是否齐全！");
+                    // 添加附件
+                    if (null != mail.getAttachments()) {
+                        for (Map.Entry<String, String> entry : mail.getAttachments().entrySet()) {
+                            String cid = entry.getKey();
+                            String filePath = entry.getValue();
+                            if (ToolsUtils.isEmpty(filePath) || ToolsUtils.isEmpty(cid)) {
+                                throw new RuntimeException("请确认每个附件的ID和地址是否齐全！");
+                            }
+                            File file = new File(filePath);
+                            if (!file.exists()) {
+                                throw new RuntimeException("附件" + filePath + "不存在！");
+                            }
+                            FileSystemResource fileResource = new FileSystemResource(file);
+                            messageHelper.addAttachment(cid, fileResource);
+                        }
                     }
-                    File file = new File(filePath);
-                    if (!file.exists()) {
-                        throw new RuntimeException("图片" + filePath + "不存在！");
-                    }
-                    FileSystemResource img = new FileSystemResource(file);
-                    messageHelper.addInline(cid, img);
+                    messageHelper.setSentDate(new Date());
+                    // 发送邮件
+                    javaMailSender.send(message);
+                    logger.info("------------发送邮件完成----------");
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                    logger.info("------------发送邮件异常----------");
                 }
             }
-
-            // 添加附件
-            if (null != mail.getAttachments()) {
-                for (Map.Entry<String, String> entry : mail.getAttachments().entrySet()) {
-                    String cid = entry.getKey();
-                    String filePath = entry.getValue();
-                    if (ToolsUtils.isEmpty(filePath) || ToolsUtils.isEmpty(cid)) {
-                        throw new RuntimeException("请确认每个附件的ID和地址是否齐全！");
-                    }
-                    File file = new File(filePath);
-                    if (!file.exists()) {
-                        throw new RuntimeException("附件" + filePath + "不存在！");
-                    }
-                    FileSystemResource fileResource = new FileSystemResource(file);
-                    messageHelper.addAttachment(cid, fileResource);
-                }
-            }
-            messageHelper.setSentDate(new Date());
-            // 发送邮件
-            javaMailSender.send(message);
-            logger.info("------------发送邮件完成----------");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            logger.info("------------发送邮件异常----------");
-        }
+        }).start();
     }
 }
